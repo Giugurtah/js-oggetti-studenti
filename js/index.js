2f7a96ab6352e4e8c1d7f06147c4cc76b93a1814
Giugurtah <c.buoncompagni.98@gmail.com>
2021-07-19 17:40:23 +0200

    ES2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //*ES1
-// const studentElement = document.getElementById("student");
+// const studentElement = document.getElementById("students");
 // var studentObject = {
 //     name : "Cristian",
 //     surname : "Buoncompagni",
@@ -12,6 +12,12 @@
 
 //*ES2
 const studentsElement = document.getElementById("students");
+const addStudentButton = document.getElementById("add_student");
+const saveDataButton = document.getElementById("save_data")
+const studentDataForm = document.getElementById("student_data");
+const studentName = document.getElementById("student_name");
+const studentSurname = document.getElementById("student_surname");
+const studentAge = document.getElementById("student_age");
 let studentsArray = [
     {
         name : "Tony",
@@ -30,9 +36,32 @@ let studentsArray = [
     },
 ];
 
-for (var i = 0; i < studentsArray.length; i++) {
-    studentsElement.innerHTML += "----------STUDENTE N°" + (i+1) + "----------<br>";
-    for (var key in studentsArray[i]) {
-        studentsElement.innerHTML += key + ": " + studentsArray[i][key] + "<br>";
+printArrayObjects(studentsArray, studentsElement);
+
+addStudentButton.addEventListener("click", function(){
+    studentDataForm.classList.remove("hide");
+})
+
+saveDataButton.addEventListener("click", function(){
+    var auxStudent = {
+        name : studentName.value,
+        surname : studentSurname.value,
+        age : parseInt(studentAge.value),
+    };
+    studentsArray.push(auxStudent);
+    studentsElement.innerHTML = "";
+    printArrayObjects(studentsArray, studentsElement);
+
+    studentName.value = "";
+    studentSurname.value = "";
+    studentAge.value = "";
+})
+
+function printArrayObjects (array, element) {
+    for (var i = 0; i < array.length; i++) {
+        element.innerHTML += "----------STUDENTE N°" + (i+1) + "----------<br>";
+        for (var key in array[i]) {
+            element.innerHTML += key + ": " + array[i][key] + "<br>";
+        }
     }
 }
