00c92f9f52661eaede900e774ea4506ca5387ac5
Giugurtah <c.buoncompagni.98@gmail.com>
2021-07-19 18:23:57 +0200

    Aggiunta validazione

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ const studentDataForm = document.getElementById("student_data");
 const studentName = document.getElementById("student_name");
 const studentSurname = document.getElementById("student_surname");
 const studentAge = document.getElementById("student_age");
-
+const WrongDataAlert = document.getElementById("wrong_data_alert");
 //Creo l'array
 let studentsArray = [
     {
@@ -51,21 +51,30 @@ addStudentButton.addEventListener("click", function(){
 
 //Salvataggio dei dati e stampa
 saveDataButton.addEventListener("click", function(){
-    //Al click viene creata una variabile oggetto ausiliaria
-    var auxStudent = {
-        name : studentName.value,
-        surname : studentSurname.value,
-        age : parseInt(studentAge.value),
-    };
-    //La variabile ausiliaria viene aggiunta all'array
-    studentsArray.push(auxStudent);
-    //Viene poi iniziallizzato il messaggio a schermo e viene poi eseguita nuovamente la funzione di stampa
-    studentsElement.innerHTML = "";
-    printArrayObjects(studentsArray, studentsElement);
+    //Controllo tramite funzioni apposite che tutti i dati siano accettabili.
+    if (validationString(studentName.value) && validationString(studentSurname.value) && validationPositiveNumber(studentAge.value)) {
+        //Se i dati sono corretti si avvia il processo di salvatagggio e stampa dati
+        //Viene creata una variabile oggetto ausiliaria
+        var auxStudent = {
+            name : studentName.value,
+            surname : studentSurname.value,
+            age : parseInt(studentAge.value),
+        };
+        //La variabile ausiliaria viene aggiunta all'array
+        studentsArray.push(auxStudent);
+        //Viene poi iniziallizzato il messaggio a schermo e viene poi eseguita nuovamente la funzione di stampa
+        studentsElement.innerHTML = "";
+        printArrayObjects(studentsArray, studentsElement);
+    
+        studentName.value = "";
+        studentSurname.value = "";
+        studentAge.value = "";
 
-    studentName.value = "";
-    studentSurname.value = "";
-    studentAge.value = "";
+        WrongDataAlert.classList.add("hide");
+    } else {
+        //Se i dati sono sbagliati allora compare un messaggio di errore
+        WrongDataAlert.classList.remove("hide");
+    }
 })
 
 //Funzione apposita per stampare tutti gli studenti dell'array
@@ -76,4 +85,21 @@ function printArrayObjects (array, element) {
             element.innerHTML += key + ": " + array[i][key] + "<br>";
         }
     }
+}
+
+//Funzione di validazione quando il dato inserito deve essere una stringa
+function validationString(string) {
+    if (!string || string.trim() == "" || !isNaN(string)) {
+        return false;
+    }
+    return true;
+}
+
+//Funzione di validazione quando il dato inserito deve essere un numero positivo.
+function validationPositiveNumber (number) {
+    //Il dato viene passato comunque come un stringa, ma controlla che possa essere successivamente convertito in un numero poitivo
+    if (!number || number.trim() == "" || isNaN(number) || number < 0) {
+        return false;
+    }
+    return true;
 }
