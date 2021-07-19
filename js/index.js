6de11ee7a56365e15f795376ded88d6d5f48705e
Giugurtah <c.buoncompagni.98@gmail.com>
2021-07-19 17:46:22 +0200

    Aggiunta commenti

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//*ES1
+// *ES1
 // const studentElement = document.getElementById("students");
 // var studentObject = {
 //     name : "Cristian",
@@ -11,13 +11,18 @@
 // }
 
 //*ES2
+//Elemento dove verranno stampati i dati degli studenti
 const studentsElement = document.getElementById("students");
+//Bottoni
 const addStudentButton = document.getElementById("add_student");
 const saveDataButton = document.getElementById("save_data")
+//Data form
 const studentDataForm = document.getElementById("student_data");
 const studentName = document.getElementById("student_name");
 const studentSurname = document.getElementById("student_surname");
 const studentAge = document.getElementById("student_age");
+
+//Creo l'array
 let studentsArray = [
     {
         name : "Tony",
@@ -36,19 +41,25 @@ let studentsArray = [
     },
 ];
 
+//Stampo l'array con apposita funzione
 printArrayObjects(studentsArray, studentsElement);
 
+//Quando viene richiesto, il form per inserire nuovi studenti compare
 addStudentButton.addEventListener("click", function(){
     studentDataForm.classList.remove("hide");
 })
 
+//Salvataggio dei dati e stampa
 saveDataButton.addEventListener("click", function(){
+    //Al click viene creata una variabile oggetto ausiliaria
     var auxStudent = {
         name : studentName.value,
         surname : studentSurname.value,
         age : parseInt(studentAge.value),
     };
+    //La variabile ausiliaria viene aggiunta all'array
     studentsArray.push(auxStudent);
+    //Viene poi iniziallizzato il messaggio a schermo e viene poi eseguita nuovamente la funzione di stampa
     studentsElement.innerHTML = "";
     printArrayObjects(studentsArray, studentsElement);
 
@@ -57,6 +68,7 @@ saveDataButton.addEventListener("click", function(){
     studentAge.value = "";
 })
 
+//Funzione apposita per stampare tutti gli studenti dell'array
 function printArrayObjects (array, element) {
     for (var i = 0; i < array.length; i++) {
         element.innerHTML += "----------STUDENTE N°" + (i+1) + "----------<br>";
